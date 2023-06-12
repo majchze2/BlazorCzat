c689b0c69d1d8319f5ea7807748783c61d2cac1c
Zenon Majchrzak/majchze2 <mzw1@tlen.pl>
2023-06-12 19:38:14 +0200

    hhuu

--- Pages/Str.razor.cs
+++ Pages/Str.razor.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.JSInterop;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Radzen;
-using Radzen.Blazor;
 
 namespace KlientRPC.Client.Pages
 {
@@ -18,16 +16,6 @@ namespace KlientRPC.Client.Pages
         [Inject]
         protected NavigationManager NavigationManager { get; set; }
 
-        [Inject]
-        protected DialogService DialogService { get; set; }
-
-        [Inject]
-        protected TooltipService TooltipService { get; set; }
-
-        [Inject]
-        protected ContextMenuService ContextMenuService { get; set; }
-
-        [Inject]
-        protected NotificationService NotificationService { get; set; }
+       
     }
 }
